a2b5ddfa57da011a803a0afbf597f4de3c867c13
SouleymaneFOMBA <babalarab2012@gmail.com>
2023-08-22 20:23:40 +0000

    changement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { navBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    navBarComponent
   ],
   imports: [
     BrowserModule,
